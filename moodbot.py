89c773a88d07ac110e644c96db276f16505a6581
simpolism <32201324+simpolism@users.noreply.github.com>
2018-10-15 00:09:19 -0400

    Add option to run selfie channel "delete posts older than 1 week" job (#1)
    * Stubbing purge command.
    
    * Drafting purge code.
    
    * Fixing purge command.
    
    * Sending message to channel upon purge.

--- moodbot.py
+++ moodbot.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3.6
 
 import argparse
+import asyncio
+import datetime
 import logging
 import sys
 
@@ -13,6 +15,8 @@ PARSER.add_argument("-d", "--debug", action="store_true",
                     help="enable additional debug logging")
 PARSER.add_argument("-l", "--logfile", type=str,
                     help="file to write log output")
+PARSER.add_argument("-S", "--selfie", action="store_true",
+                    help="run job to clear any selfie channel messages older than 7 days")
 ARGS = PARSER.parse_args()
 
 CLIENT = discord.Client()
@@ -31,6 +35,10 @@ else:
 
 @CLIENT.event
 async def on_message(message):
+    # don't take messages if running selfie job
+    if ARGS.selfie:
+        return
+
     # we do not want the bot to reply to itself
     if message.author == CLIENT.user:
         return
@@ -50,9 +58,31 @@ async def on_message(message):
             msg = "Hello {0.author.mention}".format(message)
             await CLIENT.send_message(message.channel, msg)
 
+async def purge_selfies():
+    selfie_channel = CLIENT.get_channel("500745462337241125")
+    if not selfie_channel:
+        LOGGER.error("Could not find selfie channel!")
+        return
+    now_dt = datetime.datetime.utcnow()
+    start_dt = now_dt - datetime.timedelta(weeks=1)
+    end_dt = now_dt - datetime.timedelta(weeks=2)
+    limit = 10000
+    await CLIENT.send_message(selfie_channel, "Purging selfies older than {}!".format(str(start_dt)))
+    while True:
+        LOGGER.info('Running selfie purge job!')
+        purged = await CLIENT.purge_from(selfie_channel, limit=limit, before=start_dt, after=end_dt)
+        if len(purged) < limit:
+            LOGGER.info("Purged {} messages, finished!".format(len(purged)))
+            break
+        else:
+            LOGGER.info("Purged {} messages, doing another round!".format(len(purged)))
+
 @CLIENT.event
 async def on_ready():
     LOGGER.info('Logged in as: {} / {}'.format(CLIENT.user.name, CLIENT.user.id))
+    if ARGS.selfie:
+        await purge_selfies()
+        await CLIENT.close()
 
 def main():
     LOGGER.info('Starting moodbot!')
